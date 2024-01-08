2f8e21e8a3a666b37cd09271d8e219ed3b15b09d
Ante Pišković <151842550+AnteDev00@users.noreply.github.com>
2024-01-08 17:29:34 +0100

    Update Numbers.h
    Refined

--- Numbers.h
+++ Numbers.h
@@ -1,18 +1,27 @@
 #pragma once
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 class Number
 {
 public:
 	uint64_t value;
-	uint64_t unique_factors = 2; // excluding 1 and itself
+	uint64_t unique_factors; 
 	long double compositeness;
 	string primeness;
 public:
-	Number();
-	Number(uint64_t _value): value(_value), compositeness(2/value) {}
+	Number(int _value) 
+	{
+		value = _value;
+		primeness = "not-prime";
+		compositeness = 1;
+		if (value == 1) 
+			unique_factors = 1;
+		else 
+			unique_factors = 2;
+	}
 };
 
 class Numbers
@@ -21,13 +30,14 @@ public:
 	vector<Number> numVector;
 	int size;
 public:
-	Numbers(int _size) : size(_size) {}
-
-	void FillVector(int digits);
-	void FillVector(bool randBool, int digits); 
-
+	Numbers(int _size) : size(_size) 
+	{
+		numVector.reserve(_size);
+		for (int i = 1; i < size + 1; i++)
+		{
+			numVector.emplace_back(i % (size + 1));
+		}
+	}
 	void ArePrime();
-
-	void PrintNumbers();
-	void PrintNumbers(int everyX);
+	void PrintNumbers(int msDelay);
 };
