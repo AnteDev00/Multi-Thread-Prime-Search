21c2ec8d8759f56a8dfb1d926cbf03f1fcda9f0e
Ante Pišković <151842550+AnteDev00@users.noreply.github.com>
2024-01-08 17:27:11 +0100

    Update Source.cpp
    Refined.

--- Source.cpp
+++ Source.cpp
@@ -1,54 +1,29 @@
-﻿#include <iostream>
+#include <iostream>
 #include "Numbers.h"
 
 #include <thread> 
 
-#include <cmath> // delete later
 using namespace std;
-
 int main()
 {
-    // manual input
-    /*
-	int size = 1;
-	cout << "Unesite broj Duljine Polja: ";
-	cin >> size;
-	Numbers myNums(size);
-
-	int period = 1;
-	cout << "Unesite broj Periodicnosti ispisa: ";
-	cin >> period;
-
-	int digits = 0;
-	cout << "Unesite broj Decimala Brojeva: ";
-	cin >> digits;
-
-
-	myNums.FillVector(false, digits);
-	myNums.ArePrime();
-	myNums.PrintNumbers(period);
-        
-    */
     cout.flush();
+    cout << "Testing first X numbers on primality. (don't go over ~200'000)\n\nEnter the X: ";
+    
+    int X = 0;
+    cin >> X;
+    Numbers nums(X);
 
-    // automatic 
-    for (int i = 1; i < 1001; i++)
-    {
-        auto startTime = chrono::high_resolution_clock::now();
-        cout << i << "th  sequence started.\n\n";
-        Numbers Test(10000);
-        Test.FillVector(false, 0);
-        Test.ArePrime();
-        Test.PrintNumbers(1);
-        auto endTime = chrono::high_resolution_clock::now();
-        auto duration = chrono::duration_cast<chrono::milliseconds>(endTime - startTime);
-        cout << "\n" << i << "th  sequence done" << ". Time: "<<duration.count() << " miliseconds \n\n\n" ;
-        Test.~Numbers();
-        // SLEEP / PAUSE
-        this_thread::sleep_for(chrono::milliseconds(100));
-        // Namespace::Function(Namespace::Function(Parametar))
-              
-    }
+    cout << "\nTime to calculate!\n\n";
+    nums.ArePrime();
   
+    cout << "\n\nIn 5 seconds, the numbers are going to be displayed FAST, together with the info.\n";
+    this_thread::sleep_for(std::chrono::milliseconds(5000));
+    int delayInMs = 0;
+    nums.PrintNumbers(delayInMs);
+
+    cout << "\nOver.";
+    cout.flush();
 }
+
+
 
